722fe4a1eac55d85be3508ba1d3e6023c09e3da4
ArranBrown <2323915b@student.gla.ac.uk>
2020-12-01 16:41:56 +0000

    abstracted Random and deterministic too seperate files

---
+++ notebooks/DeterministicAgent.py
@@ -0,0 +1,20 @@
+class DeterministicAgent:
+
+    def __init__(self, env, action, action_text):
+        self.num_of_actions = env.action_space.n
+        self.action = action
+        self.env = env
+        self.action_text = action_text
+        print("Agent has " + str(self.num_of_actions) + " actions and will always choose action " + str(action) + ": " + action_text)
+        
+    def get_action(self):
+        return self.action
+    
+    def get_action_text(self):
+        return self.action_text
+    
+    def get_env(self):
+        return self.env
+    
+    def get_chart_title(self):
+        return "Action = " + self.action_text
---
+++ notebooks/RandomAgent.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+class RandomAgent:
+
+    def __init__(self, env, action_text):
+        self.num_of_actions = env.action_space.n
+        self.env = env
+        self.action_text = action_text
+        print("Agent has " + str(self.num_of_actions) + " actions and will randomly select one at each step")
+        
+    def get_action(self):
+        self.action = np.random.choice(self.num_of_actions)
+        return self.action
+    
+    def get_action_text(self):
+        return self.action_text[self.action]
+    
+    def get_env(self):
+        return self.env
+    
+    def get_chart_title(self):
+        return "Random Actions : Stochastic = " + str(self.env.is_stochastic) + " , Noisy = " + str(self.env.is_noisy)
