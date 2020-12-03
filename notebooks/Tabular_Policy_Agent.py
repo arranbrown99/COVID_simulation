9b353ef888571f427d95d95bdc11935c2919daca
ArranBrown <2323915b@student.gla.ac.uk>
2020-12-03 19:55:01 +0000

    tabular

--- notebooks/Tabular_Policy_Agent.py
+++ notebooks/Tabular_Policy_Agent.py
@@ -28,7 +28,6 @@ class Tabular_Policy_Agent:
         self.env = env
         
         self.episodes = 500
-        self.print_out_every_x_episodes = int(self.episodes/50)
         self.times_exploited = 0
         
         
@@ -69,11 +68,6 @@ class Tabular_Policy_Agent:
             states,rewards = self.run_episode(epislon)
             total_reward = np.sum(rewards)
 
-#             if episode % self.print_out_every_x_episodes == 0:
-#                 print("Episode number: " + str(episode) + ". Total reward in episode: " + str(total_reward) + ". Episode executed with epsilon = " + str(epislon))
-#                 print("Average total reward in last " + str(self.print_out_every_x_episodes) + " episodes: " + str(np.mean(all_rewards[-self.print_out_every_x_episodes:])))
-#                 print("Times exploited the last episode " + str(self.times_exploited))
-#                 print("-----")
             self.times_exploited = 0
             all_total_rewards.append(total_reward)
             all_rewards.append(rewards)
